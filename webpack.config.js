8e8e1e35bfd2c1528804035d3432065c5ffb8577
vinsdesign <kelvinsaputra024@gmail.com>
2024-11-26 15:32:33 +0800

    feat: install tailwind & configure

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./assets/js/index.js", // Entry point untuk JavaScript Anda
+  output: {
+    path: path.resolve(__dirname, "apps/static/src/js"), // Output folder
+    filename: "bundle.js", // Nama file hasil
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"], // Use PostCSS to process Tailwind], // Loader untuk file CSS
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./apps/static", // Serve static files from this directory
+    hot: true, // Enable hot module replacement
+  },
+  mode: "development", // Mode: development atau production
+};
